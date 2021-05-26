e60ffd214150f696430610be4f78c7cbf051ec75
Shi Han NG <shihanng@gmail.com>
2021-05-26 22:07:20 +0900

    Extract all countries from page

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/PuerkitoBio/goquery v1.6.1
-	github.com/chromedp/chromedp v0.7.2
+	github.com/chromedp/chromedp v0.7.3-0.20210517095211-f50e6240fa87
 	golang.org/x/sys v0.0.0-20210511113859-b0526f3d8744 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -9,30 +9,34 @@ import (
 )
 
 const (
-	entryPoint           = `https://www.iso.org/obp/ui#home`
-	countryCodesSelector = `#obpui-105541713 > div > div.v-customcomponent.v-widget.v-has-width.v-has-height > div > div > div:nth-child(2) > div > div > div.v-tabsheet-content.v-tabsheet-content-header > div > div > div > div > div > div:nth-child(2) > div > div.v-slot.v-slot-xmltype > div > span:nth-child(7) > label`
-	searchSelector       = `#obpui-105541713 > div > div.v-customcomponent.v-widget.v-has-width.v-has-height > div > div > div:nth-child(2) > div > div > div.v-tabsheet-content.v-tabsheet-content-header > div > div > div > div > div > div:nth-child(2) > div > div.v-slot.v-slot-global-search.v-slot-light.v-slot-home-search > div > div.v-panel-content.v-panel-content-global-search.v-panel-content-light.v-panel-content-home-search.v-scrollable > div > div > div.v-slot.v-slot-go > div > span > span`
-	tableSelector        = `#obpui-105541713 > div > div.v-customcomponent.v-widget.v-has-width.v-has-height > div > div > div:nth-child(2) > div > div > div.v-tabsheet-content.v-tabsheet-content-header > div > div > div > div > div > div.v-slot.v-slot-borderless > div > div.v-panel-content.v-panel-content-borderless.v-scrollable > div > div > div.v-slot.v-slot-search-result-layout > div > div:nth-child(2) > div.v-grid.v-widget.country-code.v-grid-country-code.v-has-width > div.v-grid-tablewrapper > table`
-	outputFilename       = `country_codes.html`
+	entryPoint             = `https://www.iso.org/obp/ui#home`
+	countryCodesSelector   = `div > div.v-customcomponent.v-widget.v-has-width.v-has-height > div > div > div:nth-child(2) > div > div > div.v-tabsheet-content.v-tabsheet-content-header > div > div > div > div > div > div:nth-child(2) > div > div.v-slot.v-slot-xmltype > div > span:nth-child(7) > label`
+	searchSelector         = `div > div.v-customcomponent.v-widget.v-has-width.v-has-height > div > div > div:nth-child(2) > div > div > div.v-tabsheet-content.v-tabsheet-content-header > div > div > div > div > div > div:nth-child(2) > div > div.v-slot.v-slot-global-search.v-slot-light.v-slot-home-search > div > div.v-panel-content.v-panel-content-global-search.v-panel-content-light.v-panel-content-home-search.v-scrollable > div > div > div.v-slot.v-slot-go > div > span > span`
+	tableSelector          = `div > div.v-customcomponent.v-widget.v-has-width.v-has-height > div > div > div:nth-child(2) > div > div > div.v-tabsheet-content.v-tabsheet-content-header > div > div > div > div > div > div.v-slot.v-slot-borderless > div > div.v-panel-content.v-panel-content-borderless.v-scrollable > div > div > div.v-slot.v-slot-search-result-layout > div > div:nth-child(2) > div.v-grid.v-widget.v-has-width.country-code.v-grid-country-code > div.v-grid-tablewrapper > table`
+	outputFilename         = `country_codes.html`
+	resultsPerPageSelector = `div > div.v-customcomponent.v-widget.v-has-width.v-has-height > div > div > div:nth-child(2) > div > div > div.v-tabsheet-content.v-tabsheet-content-header > div > div > div > div > div > div.v-slot.v-slot-search-header > div > div:nth-child(5) > div:nth-child(3) > div > select`
+	endingRowSelector      = `div > div.v-customcomponent.v-widget.v-has-width.v-has-height > div > div > div:nth-child(2) > div > div > div.v-tabsheet-content.v-tabsheet-content-header > div > div > div > div > div > div.v-slot.v-slot-borderless > div > div.v-panel-content.v-panel-content-borderless.v-scrollable > div > div > div.v-slot.v-slot-search-result-layout > div > div:nth-child(2) > div.v-grid.v-widget.country-code.v-grid-country-code.v-has-width > div.v-grid-tablewrapper > table > tbody > tr:nth-child(242) > td:nth-child(2)`
 )
 
 func main() {
-	ctx, cancel := chromedp.NewContext(
-		context.Background(),
-	)
+	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
-	var example string
+	var htmlContent string
 	if _, err := chromedp.RunResponse(ctx,
 		chromedp.Navigate(entryPoint),
+		chromedp.WaitVisible(countryCodesSelector, chromedp.ByQuery),
 		chromedp.Click(countryCodesSelector, chromedp.NodeVisible),
 		chromedp.Click(searchSelector, chromedp.NodeVisible),
-		chromedp.OuterHTML(tableSelector, &example),
+		chromedp.SetValue(resultsPerPageSelector, "8"), // Show 300 results per page.
+		chromedp.WaitVisible(endingRowSelector),
+		chromedp.PollFunction(`() => document.querySelector("div > div.v-customcomponent.v-widget.v-has-width.v-has-height > div > div > div:nth-child(2) > div > div > div.v-tabsheet-content.v-tabsheet-content-header > div > div > div > div > div > div.v-slot.v-slot-borderless > div > div.v-panel-content.v-panel-content-borderless.v-scrollable > div > div > div.v-slot.v-slot-search-result-layout > div > div:nth-child(2) > div.v-grid.v-widget.country-code.v-grid-country-code.v-has-width > div.v-grid-tablewrapper > table > tbody > tr:nth-child(242) > td:nth-child(2)").innerText.includes("Vierges")`, nil),
+		chromedp.OuterHTML(tableSelector, &htmlContent),
 	); err != nil {
 		log.Fatal(err)
 	}
 
-	if err := ioutil.WriteFile(outputFilename, []byte(example), 0644); err != nil {
+	if err := ioutil.WriteFile(outputFilename, []byte(htmlContent), 0644); err != nil {
 		log.Fatal(err)
 	}
 }
