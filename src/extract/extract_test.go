a8b136e087481206d1aa83cb6a1f56b9045c5609
Shi Han NG <shihanng@gmail.com>
2021-06-05 12:46:14 +0900

    extract: Test ExtractAlpha2Code

--- src/extract/extract_test.go
+++ src/extract/extract_test.go
@@ -12,6 +12,21 @@ import (
 
 const testdata = "testdata/"
 
+func TestExtractAlpha2Code(t *testing.T) {
+	f, err := os.Open(testdata + "alpha2_code.html")
+	require.NoError(t, err)
+	defer f.Close()
+
+	got, err := ExtractAlpha2Code(f)
+	assert.NoError(t, err)
+
+	gotBlob, err := json.MarshalIndent(got, "", "  ")
+	assert.NoError(t, err)
+
+	g := goldie.New(t, goldie.WithNameSuffix(".golden.json"))
+	g.Assert(t, "alpha2_code", gotBlob)
+}
+
 func TestExtractDetail(t *testing.T) {
 	f, err := os.Open(testdata + "detail.html")
 	require.NoError(t, err)
