6452c588c8b4422397df4e034985606229ddb2eb
Shi Han NG <shihanng@gmail.com>
2021-06-06 14:14:33 +0900

    Save country list to db

--- main.go
+++ main.go
@@ -2,26 +2,46 @@ package main
 
 import (
 	"context"
-	"encoding/csv"
+	"errors"
 	"os"
 	"strings"
 
 	"github.com/apex/log"
 	"github.com/apex/log/handlers/cli"
-	"github.com/cockroachdb/errors"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/shihanng/country-codes/db"
 	"github.com/shihanng/country-codes/download"
 	"github.com/shihanng/country-codes/extract"
 )
 
+const dbPath = "country_code.db"
+
 func main() {
+	ctx := context.Background()
+
 	logger := log.Logger{
 		Level:   log.InfoLevel,
 		Handler: cli.New(os.Stderr),
 	}
 
-	listHTML, err := download.DownloadCountryListHTML(context.Background(), download.URL)
+	dbInstance, err := db.NewDB(ctx, dbPath)
+	if err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			logger.Info("no migration applied")
+		} else {
+			logger.WithError(err).Error("failed create new db instance")
+			return
+		}
+	}
+
+	countryTable := db.NewCountryTable(dbInstance)
+
+	logger.Info("done preparing db")
+
+	listHTML, err := download.DownloadCountryListHTML(ctx, download.URL)
 	if err != nil {
 		logger.WithError(err).Error("failed to download main list of countries")
+		return
 	}
 
 	logger.Info("done downloading html")
@@ -31,24 +51,15 @@ func main() {
 	codes, err := extract.ExtractAlpha2Code(r)
 	if err != nil {
 		logger.WithError(err).Error("failed to extract Alpha-2 codes")
+		return
 	}
 
 	logger.Info("done extracting Alpha-2 codes")
 
-	w := csv.NewWriter(os.Stdout)
-	if err := w.Write([]string{"code", "english_short_name"}); err != nil {
-		logger.WithError(errors.Wrap(err, "write csv header")).Error("failed to write csv header")
-	}
-
 	for _, code := range codes {
-		if err := w.Write([]string{code.Code, code.EnglishShortName}); err != nil {
-			logger.WithError(errors.Wrap(err, "write csv row")).Error("failed to write csv row")
+		if err := countryTable.UpsertCountry(ctx, code.Code, code.EnglishShortName); err != nil {
+			logger.WithError(err).Error("failed to register country to db")
+			return
 		}
-	}
-
-	w.Flush()
-
-	if err := w.Error(); err != nil {
-		logger.WithError(errors.Wrap(err, "flush csv writer")).Error("failed flush csv")
 	}
 }
