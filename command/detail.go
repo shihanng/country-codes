903655fb568d66292e270853485838c3b2bd1864
Shi Han NG <shihanng@gmail.com>
2021-06-08 21:34:09 +0900

    Handle multiple country codes

--- command/detail.go
+++ command/detail.go
@@ -25,47 +25,55 @@ Download detail country from https://www.iso.org/obp/ui#search into local DB.
 func (c *detailCommand) Run(args []string) int {
 	ctx := context.Background()
 
-	countryCode := "CH"
-
-	logCtx := c.logger.WithFields(log.Fields{
-		"country_code": countryCode,
-	})
-
-	detailHTML, err := download.DownloadCountryDetailHTML(ctx, download.DetailURL(countryCode))
-	if err != nil {
-		logCtx.WithError(err).Error("failed to detail information of country")
+	if len(args) != 1 {
+		c.logger.Errorf("expected 1 argument got %v", args)
 		return 1
 	}
 
-	logCtx.Info("done downloading html")
+	countryCodes := strings.Split(args[0], ",")
 
-	r := strings.NewReader(detailHTML)
+	for _, countryCode := range countryCodes {
+		logCtx := c.logger.WithFields(log.Fields{
+			"country_code": countryCode,
+		})
+		logCtx.Info("start downloading html")
 
-	detail, err := extract.ExtractDetail(r)
-	if err != nil {
-		logCtx.WithError(err).Error("failed to extract detail")
-		return 1
-	}
-
-	logCtx.Info("done extracting detail")
-
-	for _, language := range detail.Languages {
-		logCtxLang := logCtx.WithField("language", language.Alpha2)
-		logCtxLang.Info("register language")
-
-		lang := extract.Language{
-			Alpha2: strings.ToLower(language.Alpha2),
-			Alpha3: language.Alpha3,
-		}
-
-		if err := c.languageTable.UpsertLanguage(ctx, lang); err != nil {
-			logCtxLang.WithError(err).Error("failed to register language to db")
+		detailHTML, err := download.DownloadCountryDetailHTML(ctx, download.DetailURL(countryCode))
+		if err != nil {
+			logCtx.WithError(err).Error("failed to detail information of country")
 			return 1
 		}
 
-		if err := c.languageTable.SetLocalShortName(ctx, countryCode, lang.Alpha2, language.LocalShortName); err != nil {
-			logCtxLang.WithError(err).Error("failed to register language for country to db")
+		logCtx.Info("done downloading html")
+
+		r := strings.NewReader(detailHTML)
+
+		detail, err := extract.ExtractDetail(r)
+		if err != nil {
+			logCtx.WithError(err).Error("failed to extract detail")
 			return 1
+		}
+
+		logCtx.Info("done extracting detail")
+
+		for _, language := range detail.Languages {
+			logCtxLang := logCtx.WithField("language", language.Alpha2)
+			logCtxLang.Info("register language")
+
+			lang := extract.Language{
+				Alpha2: strings.ToLower(language.Alpha2),
+				Alpha3: language.Alpha3,
+			}
+
+			if err := c.languageTable.UpsertLanguage(ctx, lang); err != nil {
+				logCtxLang.WithError(err).Error("failed to register language to db")
+				return 1
+			}
+
+			if err := c.languageTable.SetLocalShortName(ctx, countryCode, lang.Alpha2, language.LocalShortName); err != nil {
+				logCtxLang.WithError(err).Error("failed to register language for country to db")
+				return 1
+			}
 		}
 	}
 
